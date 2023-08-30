26adea3f7c2e2affa2909b0dd9b127561a0a4430
BH6BAO <strongwong003@gmail.com>
2023-08-30 17:11:54 +0800

    [GPIO] update digital read

--- libraries/BouffaloLab/examples/GPIO/DigitalRead/DigitalRead.ino
+++ libraries/BouffaloLab/examples/GPIO/DigitalRead/DigitalRead.ino
@@ -1,13 +1,13 @@
 
 void setup() {
     // put your setup code here, to run once:
-    pinMode(34, INPUT);
+    pinMode(33, INPUT_PULLUP);
 }
 
 uint32_t i = 0;
 void loop() {
     // put your main code here, to run repeatedly:
-    int val = digitalRead(34);
+    int val = digitalRead(33);
     printf("hellow world %d, val = %d\r\n", i, val);
     i++;
     delay(1000);
